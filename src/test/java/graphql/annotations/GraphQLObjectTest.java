Enigmatis/graphql-java-annotations

commit 36d2bff84d2f73a118fcff4ed87adadd46f86ac1

@GraphQLField for private field also (#26)
It's now possible to add annotations on private field, so you don't have to add them to getters / setters.

src/main/java/graphql/annotations/GraphQLAnnotations.java
@@ -200,7 +200,7 @@ public class GraphQLAnnotations {
                 builder.field(field(method));
             }
         }
-        for (Field field : object.getFields()) {
+        for (Field field : object.getDeclaredFields() ) {
             boolean valid = !Modifier.isStatic(field.getModifiers()) &&
                     field.getAnnotation(GraphQLField.class) != null;
             if (valid) {

commit 92c67c68b89718b856abbf4a8adc7a63706493e9

Tests for private field and field inheritance
Fixes an inheritance issue introduced by #26

src/main/java/graphql/annotations/GraphQLAnnotations.java
@@ -200,19 +200,37 @@ public class GraphQLAnnotations {
                 builder.field(field(method));
             }
         }
-        for (Field field : object.getDeclaredFields() ) {
+
+        for (Field field : getAllFields(object).values()) {
             boolean valid = !Modifier.isStatic(field.getModifiers()) &&
                     field.getAnnotation(GraphQLField.class) != null;
             if (valid) {
                 builder.field(field(field));
             }
         }
+
         for (Class<?> iface : object.getInterfaces()) {
             if (iface.getAnnotation(GraphQLTypeResolver.class) != null) {
                 builder.withInterface((GraphQLInterfaceType) iface(iface));
             }
         }
         return builder;
+    }
+
+    protected static Map<String, Field> getAllFields(Class c) {
+        Map<String, Field> fields;
+
+        if(c.getSuperclass() != null) {
+            fields = getAllFields(c.getSuperclass());
+        } else {
+            fields = new HashMap<>();
+        }
+
+        for(Field f : c.getDeclaredFields()) {
+            fields.put(f.getName(), f);
+        }
+
+        return fields;
     }
 
 

src/test/java/graphql/annotations/GraphQLObjectTest.java
@@ -79,6 +79,14 @@ public class GraphQLObjectTest {
             return null;
         }
 
+        @GraphQLField
+        public String publicTest = "public";
+
+        @Getter
+        @Setter
+        @GraphQLField
+        private String privateTest = "private";
+
     }
 
     private static class TestDefaults {
@@ -115,7 +123,7 @@ public class GraphQLObjectTest {
     public void fields() {
         GraphQLObjectType object = GraphQLAnnotations.object(TestObject.class);
         List<GraphQLFieldDefinition> fields = object.getFieldDefinitions();
-        assertEquals(fields.size(), 5);
+        assertEquals(fields.size(), 7);
 
         fields.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
 
@@ -144,6 +152,9 @@ public class GraphQLObjectTest {
         assertEquals(fields.get(0).getName(), "deprecate");
         assertTrue(fields.get(0).isDeprecated());
         assertEquals(fields.get(0).getDeprecationReason(), "Reason");
+
+        assertEquals(fields.get(5).getName(), "privateTest");
+        assertEquals(fields.get(6).getName(), "publicTest");
 
     }
 