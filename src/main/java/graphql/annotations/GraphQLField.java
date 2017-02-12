2e6118c96a62daa3146819d986b19cc63e9da7ab
Chas Honton <chonton@elementum.com>
2017-02-11 22:21:54 -0800

    Support inheritence of GraphQLField annotation (#61)

--- src/main/java/graphql/annotations/GraphQLField.java
+++ src/main/java/graphql/annotations/GraphQLField.java
@@ -19,7 +19,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.FIELD})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface GraphQLField {
+  boolean value() default true;
 }
