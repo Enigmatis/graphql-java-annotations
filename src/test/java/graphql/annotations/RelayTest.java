44ca1f91c60cbbe7090257d0a2deebb95d4cf93c
Yarin <yarinvak@gmail.com>
2017-10-10 15:38:37 +0300

    tests fixes

--- src/test/java/graphql/annotations/RelayTest.java
+++ src/test/java/graphql/annotations/RelayTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Yurii Rashkovskii
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -44,7 +44,7 @@ public class RelayTest {
         int getI();
     }
 
-    public static class Result implements IResult{
+    public static class Result implements IResult {
         private final int i;
 
         public Result(int i) {
@@ -57,22 +57,28 @@ public class RelayTest {
     }
 
     public static class WrongReturnType {
-        @GraphQLField @GraphQLRelayMutation
+        @GraphQLField
+        @GraphQLRelayMutation
         public int doSomething() {
             return 0;
         }
     }
 
     public static class TestObject {
-        @GraphQLField @GraphQLRelayMutation
+        @GraphQLField
+        @GraphQLRelayMutation
         public Result doSomething() {
             return new Result(0);
         }
-        @GraphQLField @GraphQLRelayMutation
+
+        @GraphQLField
+        @GraphQLRelayMutation
         public Result doSomethingElse(@GraphQLName("a") @GraphQLDescription("A") int a, @GraphQLName("b") int b) {
             return new Result(a - b);
         }
-        @GraphQLField @GraphQLRelayMutation
+
+        @GraphQLField
+        @GraphQLRelayMutation
         public IResult doSomethingI() {
             return new Result(0);
         }
