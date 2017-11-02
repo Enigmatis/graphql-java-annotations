875b2904b959d029867ab58af2a4671d0d5b52b9
guy120494 <guy120494@gmail.com>
2017-11-02 23:37:42 +0200

    now "isConnection" has a ConnectionValidator interface that checks what is the validator value in the @GraphQLConnection

--- src/main/java/graphql/annotations/GraphQLAnnotations.java
+++ src/main/java/graphql/annotations/GraphQLAnnotations.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Yurii Rashkovskii
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,9 +15,7 @@
 package graphql.annotations;
 
 import graphql.TypeResolutionEnvironment;
-import graphql.annotations.connection.ConnectionDataFetcher;
-import graphql.annotations.connection.ConnectionTypeValidator;
-import graphql.annotations.connection.GraphQLConnection;
+import graphql.annotations.connection.*;
 import graphql.relay.Relay;
 import graphql.schema.*;
 import graphql.schema.GraphQLNonNull;
@@ -603,7 +601,6 @@ public class GraphQLAnnotations implements GraphQLAnnotationsProcessor {
     }
 
     private boolean isConnection(AccessibleObject obj, GraphQLOutputType type) {
-        ConnectionTypeValidator validator = new ConnectionTypeValidator();
         if (type instanceof GraphQLNonNull) {
             type = (GraphQLOutputType) ((GraphQLNonNull) type).getWrappedType();
         }
@@ -613,7 +610,8 @@ public class GraphQLAnnotations implements GraphQLAnnotationsProcessor {
                 actualType instanceof GraphQLList && TYPES_FOR_CONNECTION.stream().anyMatch(aClass ->
                 aClass.isInstance(((GraphQLList) actualType).getWrappedType()));
 
-        if(isValidGraphQLTypeForConnection) {
+        if (isValidGraphQLTypeForConnection) {
+            ConnectionValidator validator = newInstance(obj.getAnnotation(GraphQLConnection.class).validator());
             validator.validate(obj);
             return true;
         } else {
