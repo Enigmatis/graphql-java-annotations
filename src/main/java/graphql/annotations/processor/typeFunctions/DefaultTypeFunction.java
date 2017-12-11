12598d0bb018c2c3d723f89d35a7741ca7d59448
guy120494 <guy120494@gmail.com>
2017-12-11 11:24:00 +0200

    add support for byte,short,bigInteger,bigDecimal and char

--- src/main/java/graphql/annotations/processor/typeFunctions/DefaultTypeFunction.java
+++ src/main/java/graphql/annotations/processor/typeFunctions/DefaultTypeFunction.java
@@ -55,6 +55,11 @@ public class DefaultTypeFunction implements TypeFunction {
         typeFunctions.add(new FloatFunction());
         typeFunctions.add(new IntegerFunction());
         typeFunctions.add(new LongFunction());
+        typeFunctions.add(new ByteFunction());
+        typeFunctions.add(new ShortFunction());
+        typeFunctions.add(new BigIntegerFunction());
+        typeFunctions.add(new BigDecimalFunction());
+        typeFunctions.add(new CharFunction());
         typeFunctions.add(new IterableFunction(DefaultTypeFunction.this));
         typeFunctions.add(new StreamFunction(DefaultTypeFunction.this));
         typeFunctions.add(new OptionalFunction(DefaultTypeFunction.this));
