08192b8f56573769687c7d8ed01f4258088bf6ef
guy120494 <guy120494@gmail.com>
2018-04-12 13:13:06 +0300

    Use the right relay

--- src/main/java/graphql/annotations/processor/retrievers/GraphQLFieldRetriever.java
+++ src/main/java/graphql/annotations/processor/retrievers/GraphQLFieldRetriever.java
@@ -57,10 +57,10 @@ public class GraphQLFieldRetriever {
 
         boolean isConnection = ConnectionUtil.isConnection(method, outputType);
         if (isConnection) {
-            outputType = getGraphQLConnection(method, outputType, container.getRelay(), container.getTypeRegistry());
+            outputType = getGraphQLConnection(method, outputType, ConnectionUtil.getRelay(method, container), container.getTypeRegistry());
+            builder.argument(ConnectionUtil.getRelay(method, container).getConnectionFieldArguments());
         }
         builder.type(outputType);
-        handleConnectionArgument(container, builder, isConnection);
         List<GraphQLArgument> args = new ArgumentBuilder(method, typeFunction, builder, container, outputType).build();
         GraphQLFieldDefinition relayFieldDefinition = handleRelayArguments(method, container, builder, outputType, args);
         builder.description(new DescriptionBuilder(method).build())
@@ -77,8 +77,8 @@ public class GraphQLFieldRetriever {
         GraphQLType outputType = typeFunction.buildType(field.getType(), field.getAnnotatedType(), container);
         boolean isConnection = ConnectionUtil.isConnection(field, outputType);
         if (isConnection) {
-            outputType = getGraphQLConnection(field, outputType, container.getRelay(), container.getTypeRegistry());
-            builder.argument(container.getRelay().getConnectionFieldArguments());
+            outputType = getGraphQLConnection(field, outputType, ConnectionUtil.getRelay(field, container), container.getTypeRegistry());
+            builder.argument(ConnectionUtil.getRelay(field, container).getConnectionFieldArguments());
         }
 
         builder.type((GraphQLOutputType) outputType).description(new DescriptionBuilder(field).build())
@@ -112,12 +112,6 @@ public class GraphQLFieldRetriever {
             builder.argument(args);
         }
         return relayFieldDefinition;
-    }
-
-    private void handleConnectionArgument(ProcessingElementsContainer container, GraphQLFieldDefinition.Builder builder, boolean isConnection) {
-        if (isConnection) {
-            builder.argument(container.getRelay().getConnectionFieldArguments());
-        }
     }
 
     private TypeFunction getTypeFunction(Method method, ProcessingElementsContainer container) {
