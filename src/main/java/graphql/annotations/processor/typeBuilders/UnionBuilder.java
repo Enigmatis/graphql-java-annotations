978dfc7dd6e6f6d0d0eeafd50f8e63d360e0e7a6
guy120494 <guy120494@gmail.com>
2018-04-12 17:50:27 +0300

    Added custom type resolver option in union

--- src/main/java/graphql/annotations/processor/typeBuilders/UnionBuilder.java
+++ src/main/java/graphql/annotations/processor/typeBuilders/UnionBuilder.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Yurii Rashkovskii
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,17 +19,20 @@ import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLType;
 import graphql.annotations.annotationTypes.GraphQLUnion;
 import graphql.annotations.processor.ProcessingElementsContainer;
-import graphql.annotations.processor.util.ReflectionKit;
 import graphql.annotations.processor.exceptions.GraphQLAnnotationsException;
-import graphql.annotations.processor.typeFunctions.TypeFunction;
 import graphql.annotations.processor.retrievers.GraphQLObjectInfoRetriever;
+import graphql.annotations.processor.typeFunctions.TypeFunction;
 import graphql.annotations.typeResolvers.UnionTypeResolver;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLUnionType;
+import graphql.schema.TypeResolver;
 
+import java.lang.reflect.Constructor;
 import java.util.Arrays;
-import java.util.function.Function;
+import java.util.Optional;
 
+import static graphql.annotations.processor.util.ReflectionKit.constructNewInstance;
+import static graphql.annotations.processor.util.ReflectionKit.newInstance;
 import static graphql.schema.GraphQLUnionType.newUnionType;
 
 public class UnionBuilder {
@@ -65,21 +68,32 @@ public class UnionBuilder {
         TypeFunction typeFunction = container.getDefaultTypeFunction();
 
         if (typeAnnotation != null) {
-            typeFunction = ReflectionKit.newInstance(typeAnnotation.value());
+            typeFunction = newInstance(typeAnnotation.value());
         }
 
         TypeFunction finalTypeFunction = typeFunction;
-        Arrays.asList(unionAnnotation.possibleTypes()).stream()
-                .map(new Function<Class<?>, graphql.schema.GraphQLType>() {
-                    @Override
-                    public graphql.schema.GraphQLType apply(Class<?> aClass) {
-                        return finalTypeFunction.buildType(aClass, null, container);
-                    }
-                })
+        Arrays.stream(unionAnnotation.possibleTypes())
+                .map(aClass -> finalTypeFunction.buildType(aClass, null, container))
                 .map(v -> (GraphQLObjectType) v)
                 .forEach(builder::possibleType);
 
-        builder.typeResolver(new UnionTypeResolver(unionAnnotation.possibleTypes(), container));
+        TypeResolver typeResolver = getTypeResolver(container, unionAnnotation);
+
+        builder.typeResolver(typeResolver);
         return builder;
+    }
+
+    private TypeResolver getTypeResolver(ProcessingElementsContainer container, GraphQLUnion unionAnnotation) {
+        Optional<Constructor<?>> typeResolverConstructorOptional = Arrays.stream(unionAnnotation.typeResolver().getConstructors())
+                .filter(constructor -> constructor.getParameterCount() == 2 &&
+                        container.getClass().isAssignableFrom(constructor.getParameterTypes()[1]) &&
+                        Class[].class.isAssignableFrom(constructor.getParameterTypes()[0]))
+                .findFirst();
+
+        TypeResolver typeResolver;
+        typeResolver = typeResolverConstructorOptional
+                .map(constructor -> (TypeResolver) constructNewInstance(constructor, unionAnnotation.possibleTypes(), container))
+                .orElseGet(() -> new UnionTypeResolver(unionAnnotation.possibleTypes(), container));
+        return typeResolver;
     }
 }
