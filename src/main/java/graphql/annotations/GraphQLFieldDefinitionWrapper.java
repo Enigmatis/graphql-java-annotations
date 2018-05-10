584aa7837228b55ab82206889468db11700814da
yarinvak <yarinvak@gmail.com>
2018-05-10 11:07:28 +0300

    change dependency & fixed api changes

--- src/main/java/graphql/annotations/GraphQLFieldDefinitionWrapper.java
+++ src/main/java/graphql/annotations/GraphQLFieldDefinitionWrapper.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Yurii Rashkovskii
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,11 +18,13 @@ import graphql.schema.DataFetcher;
 import graphql.schema.DataFetcherFactories;
 import graphql.schema.GraphQLFieldDefinition;
 
-public  class GraphQLFieldDefinitionWrapper extends GraphQLFieldDefinition {
+public class GraphQLFieldDefinitionWrapper extends GraphQLFieldDefinition {
 
     public GraphQLFieldDefinitionWrapper(GraphQLFieldDefinition fieldDefinition) {
         super(fieldDefinition.getName(), fieldDefinition.getDescription(), fieldDefinition.getType(),
-                DataFetcherFactories.useDataFetcher((DataFetcher<?>)fieldDefinition.getDataFetcher()), fieldDefinition.getArguments(), fieldDefinition.getDeprecationReason(),
+                DataFetcherFactories.useDataFetcher((DataFetcher<?>) fieldDefinition.getDataFetcher()),
+                fieldDefinition.getArguments(), fieldDefinition.getDeprecationReason(),
+                fieldDefinition.getDirectives(),
                 fieldDefinition.getDefinition());
     }
 
