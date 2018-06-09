5ba3f15af2fca3239adc7681a181ec97bd1302a3
Yarin <yarinvak@gmail.com>
2018-06-09 21:35:00 +0300

    add annotation target

--- src/main/java/graphql/annotations/annotationTypes/GraphQLDirectives.java
+++ src/main/java/graphql/annotations/annotationTypes/GraphQLDirectives.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface GraphQLDirectives {
     Class<? extends BasicDirectiveInfo>[] value();
