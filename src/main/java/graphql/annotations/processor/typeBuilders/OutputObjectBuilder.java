2de26700d4be7a89cc625a60262fd6e268a330dd
Yarin <yarinvak@gmail.com>
2018-06-18 18:16:50 +0300

    header license

--- src/main/java/graphql/annotations/processor/typeBuilders/OutputObjectBuilder.java
+++ src/main/java/graphql/annotations/processor/typeBuilders/OutputObjectBuilder.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2016 Yurii Rashkovskii
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,8 +16,6 @@ package graphql.annotations.processor.typeBuilders;
 
 import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLTypeResolver;
-import graphql.annotations.directives.DirectiveInfoRetriever;
-import graphql.annotations.directives.DirectiveWirer;
 import graphql.annotations.processor.ProcessingElementsContainer;
 import graphql.annotations.processor.exceptions.GraphQLAnnotationsException;
 import graphql.annotations.processor.retrievers.GraphQLExtensionsHandler;
@@ -35,7 +33,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static graphql.annotations.processor.util.ObjectUtil.getAllFields;
 import static graphql.schema.GraphQLObjectType.newObject;
