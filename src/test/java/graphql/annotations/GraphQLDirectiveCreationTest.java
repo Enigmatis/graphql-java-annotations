e5fbde0d3ed8af2475514e455a80b16fac675534
yarinvak <yarinvak@gmail.com>
2018-08-02 16:07:19 +0300

    add tests

--- src/test/java/graphql/annotations/GraphQLDirectiveCreationTest.java
+++ src/test/java/graphql/annotations/GraphQLDirectiveCreationTest.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2016 Yurii Rashkovskii
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ */
 package graphql.annotations;
 
 import graphql.annotations.annotationTypes.GraphQLDescription;
@@ -11,9 +25,7 @@ import org.testng.annotations.Test;
 
 import static graphql.Scalars.GraphQLBoolean;
 import static graphql.Scalars.GraphQLString;
-import static org.testng.AssertJUnit.assertEquals;
-import static org.testng.AssertJUnit.assertNotNull;
-import static org.testng.AssertJUnit.assertNull;
+import static org.testng.AssertJUnit.*;
 import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
 public class GraphQLDirectiveCreationTest {
