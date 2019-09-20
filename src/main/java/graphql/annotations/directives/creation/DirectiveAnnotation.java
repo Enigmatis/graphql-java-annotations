284c84b24349cb3165b128ef1ba6c3aba45a484d
Yarin <yarinvak@gmail.com>
2019-09-20 18:18:20 +0300

    directives update

---
+++ src/main/java/graphql/annotations/directives/creation/DirectiveAnnotation.java
@@ -0,0 +1,13 @@
+package graphql.annotations.directives.creation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DirectiveAnnotation{
+
+}
+
---
+++ src/main/java/graphql/annotations/directives/creation/DirectiveWiring.java
@@ -0,0 +1,16 @@
+package graphql.annotations.directives.creation;
+
+import graphql.annotations.directives.AnnotationsDirectiveWiring;
+import graphql.introspection.Introspection;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface DirectiveWiring {
+    Class<? extends AnnotationsDirectiveWiring> value();
+}
