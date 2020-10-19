80af2df74113e9f992eb27ab9d419b3986b8064a
Harwinder Singh <harwinder.singh@develon.com>
2020-10-19 14:36:21 +0200

    bugfix for MethodDataFetcher:buildArg #262

---
+++ src/main/java/graphql/annotations/annotationTypes/GraphQLConstructor.java
@@ -0,0 +1,28 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ */
+package graphql.annotations.annotationTypes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/***
+ * When using object as argument of query or mutation, a HashMap Constructor is used to initialize
+ * the object fields. In order to identify the correct constructor this annotation must be used, otherwise
+ * the object fields will be null
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.CONSTRUCTOR)
+public @interface GraphQLConstructor {
+}
